a4708572bd30ca9bd592b35b041032872e2c3d4b
rajAutomtaion <raj116629@gmail.com>
2025-06-08 23:30:00 +0530

    Added new test cases

---
+++ src/test/java/stepdefs/Login_stepdefs.java
@@ -0,0 +1,47 @@
+package stepdefs;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Login_stepdefs {
+	@Given("I have launched the simplilearn application")
+	public void i_have_launched_the_simplilearn_application() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I click on the Login Link")
+	public void i_click_on_the_Login_Link() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I enter the Username")
+	public void i_enter_the_Username() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I enter the Password")
+	public void i_enter_the_Password() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I click on the Login button")
+	public void i_click_on_the_Login_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("I should be able to see the home page")
+	public void i_should_be_able_to_see_the_home_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+
+
+}
